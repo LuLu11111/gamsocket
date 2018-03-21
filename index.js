d82fdf6698d565dcb615fc4401b948e8910a7bfb
Lu Lin <lulin@lus-macbook-pro.wifi.bcit.ca>
2018-03-20 18:20:02 -0700

    add file

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ io.on("connection", function(socket){
    
     
     socket.on("disconnect", function(){
-        console.log("user has disconnected.");
+        console.log("user has disconnected");
     });
 });
 
